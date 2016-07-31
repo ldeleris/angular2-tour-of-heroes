546c1ef7ac085b3d683897f00295e1c3b08bc429
Laurent Deleris <laurent.deleris@hotmail.fr>
2016-07-31 20:46:04 +0000

    Routing partiel

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -14,4 +14,8 @@ export class HeroService {
             setTimeout(() => resolve(HEROES), 2000)
         );
     }
+    getHero(id: number) {
+        return this.getHeroes()
+            .then(heroes => heroes.find(hero => hero.id === id));
+    }
 }
